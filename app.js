01b26742e8b52f333ffd5f29442256070a5b1679
cyber-qater <73751845+cyber-qater@users.noreply.github.com>
2021-01-10 12:45:04 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,30 @@
+document.querySelector('#red').onclick = function () { 
+    document.querySelector('body').style.backgroundColor = 'red';
+}
+document.querySelector('#orange').onclick = function () { 
+    document.querySelector('body').style.backgroundColor = 'orange';
+}
+document.querySelector('#yellow').onclick = function () { 
+    document.querySelector('body').style.backgroundColor = 'yellow';
+}
+document.querySelector('#gray').onclick = function () { 
+    document.querySelector('body').style.backgroundColor = 'gray';
+}
+document.querySelector('#green').onclick = function () { 
+    document.querySelector('body').style.backgroundColor = 'green';
+}
+document.querySelector('#purple').onclick = function () { 
+    document.querySelector('body').style.backgroundColor = 'purple';
+}
+document.querySelector('#gold').onclick = function () { 
+    document.querySelector('body').style.backgroundColor = 'gold';
+}
+document.querySelector('#aqua').onclick = function () { 
+    document.querySelector('body').style.backgroundColor = 'aqua';
+}
+document.querySelector('#brown').onclick = function () { 
+    document.querySelector('body').style.backgroundColor = 'brown';
+}
+document.querySelector('#blue').onclick = function () { 
+    document.querySelector('body').style.backgroundColor = 'blue';
+}
